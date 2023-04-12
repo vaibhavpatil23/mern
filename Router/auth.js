f2e035371da2dd1a79ec4533dc1917b68bd58bc3
vaibhav <vpatil7382@gmail.com>
2023-04-12 12:48:11 +0530

    initial commit

--- Router/auth.js
+++ Router/auth.js
@@ -4,34 +4,34 @@ const router = express.Router();
 
 require("../DB/conn");
 const User = require("../models/userschema");
-const User = require("../models/userschema");
 
 router.get("/", (req, res) => {
   res.send("Hello world from the server router js");
 });
-router.post("/register", (req, res) => {
+router.post("/register",  (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
 
-  if (!name || !email || !phone || !work || !password || !cpassword){
+  if (!name || !email || !phone || !work || !password || !cpassword) {
     return res.status(422).json({ error: "piz filled the field property" });
   }
 
-User.findOne({email:email})
-.then((UserExist)=>{
-    if(UserExist){
-        return res.status(422).json({error:"Email already exist"})
-    }
-    
-    const User = new User({ name, email, phone, work, password, cpassword})
+  User.findOne({ email: email })
+    .then((UserExist) => {
+      if (UserExist) {
+        return res.status(422).json({ error: "Email already exist" });
+      }
 
-    User.save().then(()=>{
-        res.status(201).json({message:'user register successfuly'})
-    }).catch((err)=> res.status(500).json({error:"failed to register"}))
-}).catch(err => {console.log(err);
-})
+      const User = new User({ name, email, phone, work, password, cpassword });
 
+      User.save()
+        .then(() => {
+          res.status(201).json({ message: "user register successfuly" });
+        })
+        .catch((err) => res.status(500).json({ error: "failed to register" }));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-
-
 
 module.exports = router;
