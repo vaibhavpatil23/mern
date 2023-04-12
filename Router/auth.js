19e2c9995f6cc47497b5fd4f136d8429fcdb8a5c
vaibhav <vpatil7382@gmail.com>
2023-04-12 18:20:52 +0530

    chang the auth.js file

--- Router/auth.js
+++ Router/auth.js
@@ -8,30 +8,67 @@ const User = require("../models/userschema");
 router.get("/", (req, res) => {
   res.send("Hello world from the server router js");
 });
-router.post("/register",  (req, res) => {
+// router.post("/register",  (req, res) => {
+//   const { name, email, phone, work, password, cpassword } = req.body;
+
+//   if (!name || !email || !phone || !work || !password || !cpassword) {
+//     return res.status(422).json({ error: "piz filled the field property" });
+//   }
+
+//   User.findOne({ email: email })
+//     .then((UserExist) => {
+//       if (UserExist) {
+//         return res.status(422).json({ error: "Email already exist" });
+//       }
+//       const User = new User({ name, email, phone, work, password, cpassword });
+//       User.save()
+//         .then(() => {
+//           res.status(201).json({ message: "user register successfuly" });
+//         })
+//         .catch((err) => res.status(500).json({ error: "failed to register" }));
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// async code
+
+router.post("/register", async (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
 
   if (!name || !email || !phone || !work || !password || !cpassword) {
     return res.status(422).json({ error: "piz filled the field property" });
   }
+  try {
+    const UserExist = await User.findOne({ email: email });
+    if (UserExist) {
+      return res.status(422).json({ error: "email alredy exist" });
+    }
+    const user = new user({ name, email, phone, work, password, cpassword });
+    await user.save();
+    res.status(201).json({ messege: "user register successfully" });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-  User.findOne({ email: email })
-    .then((UserExist) => {
-      if (UserExist) {
-        return res.status(422).json({ error: "Email already exist" });
-      }
+// LOGIN
 
-      const User = new User({ name, email, phone, work, password, cpassword });
+router.post("/signin", async (req, res) => {
+  // console.log(req.body)
+  // res.json({messege:"awesome"})
+  try {
+    const { email, password } = req.body;
 
-      User.save()
-        .then(() => {
-          res.status(201).json({ message: "user register successfuly" });
-        })
-        .catch((err) => res.status(500).json({ error: "failed to register" }));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    if (!email || !password) {
+      return res.status(400).json({ error: "plz filled the data" });
+    }
+    const userLogin = await User.findOne({ email: email });
+    console.log(User);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
