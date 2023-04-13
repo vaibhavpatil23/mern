0746b1208e3fadaf9776e9decb65dd3e9a4297d7
vaibhav <vpatil7382@gmail.com>
2023-04-13 13:06:51 +0530

    packeg download

--- models/userschema.js
+++ models/userschema.js
@@ -25,17 +25,40 @@ const userschema = new mongoose.Schema({
     type: String,
     require: true,
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        require: true,
+      },
+    },
+  ],
 });
 const User = mongoose.model("USER", userschema);
 
 module.exports = User;
 userschema.pre("save", async function (next) {
-  console.log("hii i am pre")
+  console.log("hii i am pre");
   if (this.isModified("password")) {
-    console.log("hii i am pre password")
+    console.log("hii i am pre password");
 
     this.password = bcrypt.hash(this.password, 12);
     this.cpassword = bcrypt.hash(this.cpassword, 12);
   }
   next();
 });
+
+userschema.method.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.token.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const user = mongoose.model("USER", userschema);
+
+module.exports = User;
