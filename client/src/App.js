89374430e488cd0b222f3cdecad2b8a0f6cbbd19
vaibhav <vpatil7382@gmail.com>
2023-04-14 18:48:03 +0530

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
       <h1>Hello word</h1>
     </div>
-  )
-}
-
-export default App
-
+  );
+};
+export default App;
